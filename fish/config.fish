f27ae1b16b0154fb1154a8948a1f231d281fe487
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-01-31 20:57:51 +1100

    🐛 Fixed path variable in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
-# PATH
-set -Ux PATH $HOME/bin:/usr/local/bin $PATH
-set -Ux PATH /usr/local/Cellar/llvm/10.0.0_1/bin $PATH
-set -Ux PATH $HOME/Documents/Programming/bin $PATH
-set -Ux PATH $HOME/.local/bin $PATH
-set -Ux PATH $HOME/.composer/vendor/bin $PATH
+# fish_user_paths
+set -Ux fish_user_paths $HOME/bin:/usr/local/bin $fish_user_paths
+set -Ux fish_user_paths /usr/local/Cellar/llvm/10.0.0_1/bin $fish_user_paths
+set -Ux fish_user_paths $HOME/Documents/Programming/bin $fish_user_paths
+set -Ux fish_user_paths $HOME/.local/bin $fish_user_paths
+set -Ux fish_user_paths $HOME/.composer/vendor/bin $fish_user_paths
 
 # Variables
 set FORMAT "ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"
