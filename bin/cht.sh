10082d339dff4087264d0dedc0f9e3373cac630d
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-09-14 15:14:34 +1000

    bin: add cheat sh

---
+++ bin/cht.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+languages=`echo "cpp c typescript javascript nodejs" | tr ' ' '\n'`
+core_utils=`echo "xargs find mv sed" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+
+read -p "query: " query
+
+echo $selected
+echo $query
+
+if printf $languages | grep -qs $selected; then
+  tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+else
+  tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+fi
+
