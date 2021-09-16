60a703eb7baa043154437d362f3e71824b408a19
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-09-16 14:24:25 +1000

    bin: fix prinf

--- bin/cht.sh
+++ bin/cht.sh
@@ -7,11 +7,9 @@ selected=`printf "$languages\n$core_utils" | fzf`
 
 read -p "query: " query
 
-echo $selected
-echo $query
-
-if printf $languages | grep -qs $selected; then
-  tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+if printf "$languages" | grep -qs $selected; then
+  processed_query=`echo $query | tr ' ' '+'`
+  tmux neww bash -c "curl cht.sh/$selected/$processed_query & while [ : ]; do sleep 1; done"
 else
   tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 fi
