6af434c33bc381828c8d495f1945428f13046684
Nam Nguyen <nguyenvietnam2401@gmail.com>
2022-09-08 12:33:39 +1000

    nvim: use gruvbox light theme

--- nvim/.config/nvim/after/plugin/theme.lua
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -1,11 +1,12 @@
 vim.cmd([[
   let g:gruvbox_contrast_dark='medium'
-  let g:gruvbox_contrast_light='hard'
-  let g:gruvbox_transparent_bg=1
+  let g:gruvbox_contrast_light='medium'
+  let g:gruvbox_transparent_bg=0
+  set background=light
   colorscheme gruvbox
 
-  " let g:tokyonight_transparent = 1
-  " let g:tokyonight_transparent_sidebar = 1
+  " let g:tokyonight_transparent=1
+  " let g:tokyonight_transparent_sidebar=1
   " colorscheme tokyonight
 
   hi! NonText ctermbg=NONE guibg=NONE guifg=NONE ctermfg=NONE
