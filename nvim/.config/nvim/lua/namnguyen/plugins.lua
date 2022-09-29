0c40b0469f866fc635989c821f6dd2a0cfe6746f
Nam Nguyen <nguyenvietnam2401@gmail.com>
2022-09-29 10:15:27 +1000

    nvim: update markdown-preview.nvim

--- nvim/.config/nvim/lua/namnguyen/plugins.lua
+++ nvim/.config/nvim/lua/namnguyen/plugins.lua
@@ -68,10 +68,8 @@ return require("packer").startup(function(use)
     -- use("sheerun/vim-polyglot")
 
     -- Markdown
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
     -- Telescope
     use {
