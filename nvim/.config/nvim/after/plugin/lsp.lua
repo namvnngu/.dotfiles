2b76695332cca00d2a982d0dac1561db62460309
Nam Nguyen <nguyenvietnam2401@gmail.com>
2022-10-17 10:01:14 +1100

    nvim: update cmp_nvim_lsp break changes

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -59,7 +59,7 @@ lsp_installer.on_server_ready(function(server)
 		settings = {
 			format = { enable = true },
 		},
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+		capabilities = require("cmp_nvim_lsp").default_capabilities()
 	}
 
 	if server.name == "eslint" then
