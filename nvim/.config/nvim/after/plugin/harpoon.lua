89497b5fa4f9bed52ce26859708a1475215d0d2c
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-02-09 13:59:32 +1100

    nvim: expand width of harpoon

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -1,6 +1,13 @@
+local harpoon = require('harpoon')
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 local noremap = require('utils.keymap').noremap
+
+harpoon.setup({
+  menu = {
+    width = vim.api.nvim_win_get_width(0) - 20,
+  },
+})
 
 noremap('<leader>ha', mark.add_file)
 noremap('<leader>ho', ui.toggle_quick_menu)
