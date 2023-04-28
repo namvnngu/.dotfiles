a6d028e05cb460def87f856edb92d3105b733de5
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-04-28 11:44:13 +1000

    vim: add vim with built-in configs

--- nvim/.config/nvim/lua/namnguyen/remap.lua
+++ nvim/.config/nvim/lua/namnguyen/remap.lua
@@ -5,28 +5,28 @@ local xnoremap = require('utils.keymap').xnoremap
 local inoremap = require('utils.keymap').inoremap
 local map = require('utils.keymap').map
 
-nnoremap('<leader>h', '<cmd>wincmd h<CR>')
-nnoremap('<leader>j', '<cmd>wincmd j<CR>')
-nnoremap('<leader>k', '<cmd>wincmd k<CR>')
-nnoremap('<leader>l', '<cmd>wincmd l<CR>')
+nnoremap('<leader>h', ':wincmd h<CR>')
+nnoremap('<leader>j', ':wincmd j<CR>')
+nnoremap('<leader>k', ':wincmd k<CR>')
+nnoremap('<leader>l', ':wincmd l<CR>')
 
-nnoremap('<Up>', '<cmd>resize -2<CR>')
-nnoremap('<Down>', '<cmd>resize +2<CR>')
-nnoremap('<Right>', '<cmd>vertical resize +2<CR>')
-nnoremap('<Left>', '<cmd>vertical resize -2<CR>')
+nnoremap('<Up>', ':resize -2<CR>')
+nnoremap('<Down>', ':resize +2<CR>')
+nnoremap('<Right>', ':vertical resize +2<CR>')
+nnoremap('<Left>', ':vertical resize -2<CR>')
 
-nnoremap('<leader>qq', '<cmd>q<CR>')
+nnoremap('<leader>qq', ':q<CR>')
 
-nnoremap('<leader>ay', '<cmd>%y+<CR>')
+nnoremap('<leader>ay', ':%y+<CR>')
 
-nnoremap('<leader>bn', '<cmd>bnext<CR>')
-nnoremap('<leader>bp', '<cmd>bprevious<CR>')
-nnoremap('<leader>bd', '<cmd>bdelete<CR>')
-nnoremap('<leader>ba', '<cmd>bufdo bd<CR>')
-nnoremap('<leader>be', '<cmd>%bd|e#<CR>')
+nnoremap('<leader>bn', ':bnext<CR>')
+nnoremap('<leader>bp', ':bprevious<CR>')
+nnoremap('<leader>bd', ':bdelete<CR>')
+nnoremap('<leader>ba', ':bufdo bd<CR>')
+nnoremap('<leader>be', ':%bd|e#<CR>')
 
-vnoremap('J', "<cmd>m '>+1<CR>gv=gv")
-vnoremap('K', "<cmd>m '<-2<CR>gv=gv")
+vnoremap('J', ":m '>+1<CR>gv=gv")
+vnoremap('K', ":m '<-2<CR>gv=gv")
 
 -- Yank til the end
 nnoremap('Y', 'y$')
@@ -39,10 +39,10 @@ nnoremap('<C-d>', '<C-d>zz')
 nnoremap('<C-u>', '<C-u>zz')
 
 -- Quicklist
-nnoremap('<C-k>', '<cmd>cnext<CR>zz')
-nnoremap('<C-j>', '<cmd>cprev<CR>zz')
-nnoremap('<leader>K', '<cmd>lnext<CR>zz')
-nnoremap('<leader>J', '<cmd>lprev<CR>zz')
+nnoremap('<C-k>', ':cnext<CR>zz')
+nnoremap('<C-j>', ':cprev<CR>zz')
+nnoremap('<leader>K', ':lnext<CR>zz')
+nnoremap('<leader>J', ':lprev<CR>zz')
 
 -- Keybindings for tab navigation with leader and number
 noremap('<leader>1', '1gt')
@@ -56,7 +56,7 @@ noremap('<leader>8', '8gt')
 noremap('<leader>9', '9gt')
 
 -- Toggle spelling checker
-map('<F4>', '<cmd>setlocal spell! spelllang=en_us<CR>')
+map('<F4>', ':setlocal spell! spelllang=en_us<CR>')
 
 -- Replace currently selected text with default register without yanking it
 xnoremap('<leader>p', '"_dP')
@@ -72,7 +72,7 @@ inoremap('<C-c>', '<Esc>')
 nnoremap('<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make file executable
-noremap('<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
+noremap('<leader>x', ':!chmod +x %<CR>', { silent = true })
 
 -- Avoid using arrow keys
 -- noremap("<Up>", "<NOP>")
---
+++ vim/.vimrc
@@ -0,0 +1,116 @@
+" OPTIONS
+syntax on
+filetype plugin indent on
+let mapleader = " "
+
+set viewoptions-=curdir
+set backspace+=nostop
+
+set breakindent
+set clipboard=unnamed
+set completeopt=menu,menuone,noselect
+set copyindent
+set expandtab
+set fileencoding=utf-8
+set history=100
+set ignorecase
+set infercase
+set laststatus=3
+set linebreak
+set number
+set preserveindent
+set pumheight=100
+set relativenumber
+set scrolloff=8
+set shiftwidth=2
+set showtabline=2
+set sidescrolloff=8
+set signcolumn=yes
+set smartcase
+set smartindent
+set splitbelow
+set splitright
+set tabstop=2
+set termguicolors
+set timeoutlen=500
+set noundofile
+set updatetime=300
+set virtualedit=block
+set nowrap
+set nowritebackup
+set autoread
+set hlsearch
+set incsearch
+set colorcolumn=80
+set noswapfile
+set nobackup
+
+" MAPPINGS
+nnoremap <leader>h :wincmd h<CR>
+nnoremap <leader>j :wincmd j<CR>')
+nnoremap <leader>k :wincmd k<CR>')
+nnoremap <leader>l :wincmd l<CR>')
+
+nnoremap <Up> :resize -2<CR>
+nnoremap <Down> :resize +2<CR>
+nnoremap <Right> :vertical resize +2<CR>
+nnoremap <Left> :vertical resize -2<CR>
+
+nnoremap <leader>qq :q<CR>
+
+nnoremap <leader>ay :%y+<CR>
+
+nnoremap <leader>bn :bnext<CR>
+nnoremap <leader>bp :bprevious<CR>
+nnoremap <leader>bd :bdelete<CR>
+nnoremap <leader>ba :bufdo bd<CR>
+nnoremap <leader>be :%bd\|e#<CR>
+
+vnoremap J :m '>+1<CR>gv=gv
+vnoremap K :m '<-2<CR>gv=gv
+
+" Yank til the end
+nnoremap Y y$
+
+" Keeping it centered and stable
+nnoremap J mzJ`z
+nnoremap n nzzzv
+nnoremap N Nzzzv
+nnoremap <C-d> <C-d>zz')
+nnoremap <C-u> <C-u>zz')
+
+" Quicklist
+nnoremap <C-k> :cnext<CR>zz
+nnoremap <C-j> :cprev<CR>zz
+nnoremap <leader>K :lnext<CR>zz
+nnoremap <leader>J :lprev<CR>zz
+
+" Keybindings for tab navigation with leader and number
+noremap <leader>1 1gt
+noremap <leader>2 2gt
+noremap <leader>3 3gt
+noremap <leader>4 4gt
+noremap <leader>5 5gt
+noremap <leader>6 6gt
+noremap <leader>7 7gt
+noremap <leader>8 8gt
+noremap <leader>9 9gt
+
+" Toggle spelling checker
+map <F4> :setlocal spell! spelllang=en_us<CR>
+
+" Replace currently selected text with default register without yanking it
+xnoremap <leader>p "_dP
+
+" Delete without yanking
+nnoremap <leader>d "_d
+vnoremap <leader>d "_d
+
+" This is going to get me cancelled
+inoremap <C-c> <Esc>
+
+" Replace
+nnoremap <leader>s :%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>
+
+" Make file executable
+noremap <silent> <leader>x :!chmod +x %<CR>
