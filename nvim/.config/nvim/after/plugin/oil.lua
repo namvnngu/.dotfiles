1c1bf5b9f43a0ca468d3b7bbfbfd2de4fd61a551
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-05-22 00:06:23 +1000

    nvim: show hidden in oil.nvim

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,6 +1,10 @@
 local oil = require('oil')
 local nnoremap = require('utils.keymap').nnoremap
 
-oil.setup()
+oil.setup({
+  view_options = {
+    show_hidden = true,
+  },
+})
 
 nnoremap('-', oil.open, { desc = 'Open parent directory' })
