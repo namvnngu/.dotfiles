fdfa82a40d79c72512995ff641b971c0072abcc4
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-06-06 22:42:04 +1000

    bin+lsp: disable eslint_d format on save

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -109,7 +109,7 @@ null_ls.setup(merge(common_setup, {
   sources = {
     -- Eslint
     null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.eslint_d,
+    -- null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.diagnostics.eslint_d.with({
       condition = function(utils)
         return utils.root_has_file({
