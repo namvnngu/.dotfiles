fac3ea54297baf80f758d66d0079e92ebb420b50
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-07-18 12:55:17 +1000

    nvim: update the order of mini.starter

--- nvim/.config/nvim/after/plugin/mini.lua
+++ nvim/.config/nvim/after/plugin/mini.lua
@@ -2,8 +2,8 @@ require('mini.comment').setup()
 
 require('mini.starter').setup({
   items = {
-    { action = 'Oil', name = 'Explorer', section = 'Browsing' },
     { action = 'Telescope git_files', name = 'Git Files', section = 'Browsing' },
+    { action = 'Oil', name = 'Explorer', section = 'Browsing' },
     { action = 'Telescope file_browser', name = 'File Browser', section = 'Browsing' },
     require('mini.starter').sections.recent_files(5, true),
   },
