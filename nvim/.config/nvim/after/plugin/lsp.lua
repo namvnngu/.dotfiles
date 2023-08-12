d2fa721fd07f4f867cda6d518ef3c4acfc224ecc
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-08-13 00:26:50 +1000

    nvim: fix on_attach for null-ls

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -40,7 +40,6 @@ require('mason-lspconfig').setup({
 })
 
 local lspconfig = require('lspconfig')
-local null_ls = require('null-ls')
 
 local merge = require('utils.table').merge
 local nnoremap = require('utils.keymap').nnoremap
@@ -52,87 +51,79 @@ nnoremap('<space>dn', vim.diagnostic.goto_next)
 nnoremap('<space>dl', vim.diagnostic.setloclist)
 nnoremap('<space>dq', vim.diagnostic.setqflist)
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    local bufnr = ev.buf
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+local common_on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  if client.server_capabilities.completionProvider then
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+  end
+  if client.server_capabilities.definitionProvider then
+    vim.bo[bufnr].tagfunc = 'v:lua.vim.lsp.tagfunc'
+  end
 
-    -- Enable completion triggered by <c-x><c-o>
-    if client.server_capabilities.completionProvider then
-      vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-    end
-    if client.server_capabilities.definitionProvider then
-      vim.bo[bufnr].tagfunc = 'v:lua.vim.lsp.tagfunc'
-    end
+  -- Buffer local mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local opts = { buffer = bufnr }
+  nnoremap('gD', vim.lsp.buf.declaration, opts)
+  nnoremap('gd', vim.lsp.buf.definition, opts)
+  nnoremap('K', vim.lsp.buf.hover, opts)
+  nnoremap('gi', vim.lsp.buf.implementation, opts)
+  nnoremap('gK', vim.lsp.buf.signature_help, opts)
+  nnoremap('<space>D', vim.lsp.buf.type_definition, opts)
+  nnoremap('<space>rn', vim.lsp.buf.rename, opts)
+  nnoremap('<space>ca', vim.lsp.buf.code_action, opts)
+  nnoremap('gr', vim.lsp.buf.references, opts)
+  nnoremap('<space>cl', vim.lsp.codelens.run, opts)
+  nnoremap('<space>cr', vim.lsp.codelens.refresh, opts)
+  nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+  nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  nnoremap('<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts)
+  nnoremap('<space>F', function()
+    vim.lsp.buf.format({
+      async = true,
+      bufnr = bufnr,
+      filter = function(formatting_client)
+        return formatting_client.name == 'null-ls'
+      end,
+    })
+  end, opts)
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = bufnr }
-    nnoremap('gD', vim.lsp.buf.declaration, opts)
-    nnoremap('gd', vim.lsp.buf.definition, opts)
-    nnoremap('K', vim.lsp.buf.hover, opts)
-    nnoremap('gi', vim.lsp.buf.implementation, opts)
-    nnoremap('gK', vim.lsp.buf.signature_help, opts)
-    nnoremap('<space>D', vim.lsp.buf.type_definition, opts)
-    nnoremap('<space>rn', vim.lsp.buf.rename, opts)
-    nnoremap('<space>ca', vim.lsp.buf.code_action, opts)
-    nnoremap('gr', vim.lsp.buf.references, opts)
-    nnoremap('<space>cl', vim.lsp.codelens.run, opts)
-    nnoremap('<space>cr', vim.lsp.codelens.refresh, opts)
-    nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    nnoremap('<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    nnoremap('<space>F', function()
-      vim.lsp.buf.format({
-        async = true,
-        bufnr = bufnr,
-        filter = function()
-          return client.name == 'null-ls'
-        end,
-      })
-    end, opts)
+  -- Format on save
+  -- local lsp_formatting_augroup = vim.api.nvim_create_augroup('LspFormatting', { clear = true })
+  -- if client.server_capabilities.documentFormattingProvider then
+  --   vim.api.nvim_clear_autocmds({ group = lsp_formatting_augroup, buffer = bufnr })
+  --   vim.api.nvim_create_autocmd('BufWritePre', {
+  --     group = lsp_formatting_augroup,
+  --     buffer = bufnr,
+  --     callback = function()
+  --       vim.lsp.buf.format({
+  --         async = true,
+  --         bufnr = bufnr,
+  --         filter = function(formatting_client)
+  --           return formatting_client.name == 'null-ls'
+  --         end,
+  --       })
+  --     end,
+  --   })
+  -- end
 
-    -- Format on save
-    -- local lsp_formatting_augroup = vim.api.nvim_create_augroup('LspFormatting', { clear = true })
-    -- if client.server_capabilities.documentFormattingProvider then
-    --   vim.api.nvim_clear_autocmds({ group = lsp_formatting_augroup, buffer = bufnr })
-    --   vim.api.nvim_create_autocmd('BufWritePre', {
-    --     group = lsp_formatting_augroup,
-    --     buffer = bufnr,
-    --     callback = function()
-    --       vim.lsp.buf.format({
-    --         async = true,
-    --         bufnr = bufnr,
-    --         filter = function()
-    --           return client.name == 'null-ls'
-    --         end,
-    --       })
-    --     end,
-    --   })
-    -- end
-
-    -- Highlight
-    local lsp_highlight_augroup = vim.api.nvim_create_augroup('LspHighlight', { clear = true })
-    if client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_clear_autocmds({ group = lsp_highlight_augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        group = lsp_highlight_augroup,
-        buffer = bufnr,
-        callback = vim.lsp.buf.document_highlight,
-      })
-      vim.api.nvim_create_autocmd('CursorMoved', {
-        group = lsp_highlight_augroup,
-        buffer = bufnr,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-  end,
-})
+  -- Highlight
+  local lsp_highlight_augroup = vim.api.nvim_create_augroup('LspHighlight', { clear = true })
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_clear_autocmds({ group = lsp_highlight_augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      group = lsp_highlight_augroup,
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd('CursorMoved', {
+      group = lsp_highlight_augroup,
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
+end
 
 local common_setup = {
   capabilities = require('cmp_nvim_lsp').default_capabilities(),
@@ -141,7 +132,9 @@ local common_setup = {
   },
 }
 
+local null_ls = require('null-ls')
 null_ls.setup(merge(common_setup, {
+  on_attach = common_on_attach,
   sources = {
     -- Eslint
     null_ls.builtins.code_actions.eslint_d,
@@ -195,7 +188,16 @@ null_ls.setup(merge(common_setup, {
   },
 }))
 
-local installed_servers = require('mason-lspconfig').get_installed_servers();
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local bufnr = ev.buf
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    common_on_attach(client, bufnr)
+  end,
+})
+
+local installed_servers = require('mason-lspconfig').get_installed_servers()
 for _, server in ipairs(installed_servers) do
   lspconfig[server].setup(common_setup)
 end
