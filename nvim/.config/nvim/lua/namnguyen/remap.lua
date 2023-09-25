88c64a3d14f725686e82eae5c5b9b9071784b282
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-09-25 15:16:24 +1000

    nvim: update keymaps

--- nvim/.config/nvim/lua/namnguyen/remap.lua
+++ nvim/.config/nvim/lua/namnguyen/remap.lua
@@ -6,33 +6,50 @@ local inoremap = require('utils.keymap').inoremap
 local tnoremap = require('utils.keymap').tnoremap
 local map = require('utils.keymap').map
 
-nnoremap('<leader>h', ':wincmd h<CR>')
-nnoremap('<leader>j', ':wincmd j<CR>')
-nnoremap('<leader>k', ':wincmd k<CR>')
-nnoremap('<leader>l', ':wincmd l<CR>')
+-- Move to window
+nnoremap('<leader>h', '<cmd>wincmd h<cr>')
+nnoremap('<leader>j', '<cmd>wincmd j<cr>')
+nnoremap('<leader>k', '<cmd>wincmd k<cr>')
+nnoremap('<leader>l', '<cmd>wincmd l<cr>')
 
-nnoremap('<Up>', ':resize -2<CR>')
-nnoremap('<Down>', ':resize +2<CR>')
-nnoremap('<Right>', ':vertical resize +2<CR>')
-nnoremap('<Left>', ':vertical resize -2<CR>')
+-- Resize window
+nnoremap('<Up>', '<cmd>resize -2<cr>')
+nnoremap('<Down>', '<cmd>resize +2<cr>')
+nnoremap('<Right>', '<cmd>vertical resize +2<cr>')
+nnoremap('<Left>', '<cmd>vertical resize -2<cr>')
 
-nnoremap('<leader>qq', ':q<CR>')
-
-nnoremap('<leader>ya', ':%y+<CR>')
+-- Yanking
+nnoremap('<leader>ya', '<cmd>%y+<cr>')
 nnoremap('<leader>y', '"+y')
 vnoremap('<leader>y', '"+y')
--- Yank til the end
-nnoremap('<leader>Y', '"+y$')
+nnoremap('<leader>Y', '"+y$', { desc = 'Yank til the end' })
 nnoremap('Y', 'y$')
 
-nnoremap('<leader>bn', ':bnext<CR>')
-nnoremap('<leader>bp', ':bprevious<CR>')
-nnoremap('<leader>bd', ':bdelete<CR>')
-nnoremap('<leader>ba', ':bufdo bd<CR>')
-nnoremap('<leader>be', ':%bd|e#<CR>')
+-- Buffers
+nnoremap('<leader>bn', '<cmd>bnext<cr>')
+nnoremap('<leader>bp', '<cmd>bprevious<cr>')
+nnoremap('<leader>bd', '<cmd>bdelete<cr>')
+nnoremap('<leader>ba', '<cmd>bufdo bd<cr>')
+nnoremap('<leader>be', '<cmd>%bd|e#<cr>')
 
-vnoremap('J', ":m '>+1<CR>gv=gv")
-vnoremap('K', ":m '<-2<CR>gv=gv")
+-- Better indenting
+vnoremap('<', '<gv')
+vnoremap('>', '>gv')
+
+-- Move lines
+nnoremap('<leader>md', '<cmd>m .+1<cr>==')
+nnoremap('<leader>mu', '<cmd>m .-2<cr>==')
+inoremap('<A-j>', '<esc><cmd>m .+1<cr>==gi')
+inoremap('<A-k>', '<esc><cmd>m .-2<cr>==gi')
+vnoremap('J', ":m '>+1<cr>gv=gv")
+vnoremap('K', ":m '<-2<cr>gv=gv")
+
+-- Clear search with <esc>
+nnoremap('<esc>', '<cmd>noh<cr><esc>')
+inoremap('<esc>', '<cmd>noh<cr><esc>')
+
+-- New file
+nnoremap('<leader>fn', '<cmd>enew<cr>')
 
 -- Keeping it centered and stable
 nnoremap('J', 'mzJ`z')
@@ -42,12 +59,12 @@ nnoremap('<C-d>', '<C-d>zz')
 nnoremap('<C-u>', '<C-u>zz')
 
 -- Quickfix list
-nnoremap('<C-k>', ':cnext<CR>zz')
-nnoremap('<C-j>', ':cprev<CR>zz')
+nnoremap('<C-k>', '<cmd>cnext<cr>zz')
+nnoremap('<C-j>', '<cmd>cprev<cr>zz')
 
 -- Location list
-nnoremap('<leader>K', ':lnext<CR>zz')
-nnoremap('<leader>J', ':lprev<CR>zz')
+nnoremap('<leader>K', '<cmd>lnext<cr>zz')
+nnoremap('<leader>J', '<cmd>lprev<cr>zz')
 
 -- Keybindings for tab navigation with leader and number
 noremap('<leader>1', '1gt')
@@ -61,7 +78,7 @@ noremap('<leader>8', '8gt')
 noremap('<leader>9', '9gt')
 
 -- Toggle spelling checker
-map('<F4>', ':setlocal spell! spelllang=en_us<CR>')
+map('<F4>', '<cmd>setlocal spell! spelllang=en_us<cr>')
 
 -- Replace currently selected text with default register without yanking it
 xnoremap('<leader>p', '"_dP')
@@ -71,20 +88,19 @@ nnoremap('<leader>d', '"_d')
 vnoremap('<leader>d', '"_d')
 
 -- This is going to get me cancelled
-inoremap('<C-c>', '<Esc>')
+inoremap('<C-c>', '<esc>')
 
 -- Replace
 nnoremap('<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make file executable
-noremap('<leader>x', ':!chmod +x %<CR>', { silent = true })
+noremap('<leader>x', '<cmd>!chmod +x %<cr>', { silent = true })
 
 -- Exit terminal mode
-tnoremap('<Esc>', [[<C-\><C-n>]])
+tnoremap('<esc>', [[<C-\><C-n>]])
 
--- Better indenting
-vnoremap("<", "<gv")
-vnoremap(">", ">gv")
+-- Quite all
+nnoremap('<leader>qq', '<cmd>q<cr>')
 
 -- Avoid using arrow keys
 -- noremap("<Up>", "<NOP>")
