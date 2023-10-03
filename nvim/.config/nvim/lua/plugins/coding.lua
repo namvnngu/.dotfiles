bd25ab81dc7a24976e4d718d38612c0b25f9ea20
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-10-03 17:30:24 +1100

    nvim: disable sync scolling in markdown preview

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -181,6 +181,7 @@ return {
     build = "cd app && npm install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_preview_options = { disable_sync_scroll = true }
     end,
     ft = { "markdown" },
   },
