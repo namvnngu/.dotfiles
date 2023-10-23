b4429e9c422460265e1df5ec7795dc5000b124fd
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-10-24 10:53:25 +1100

    nvim: improve loading mini.align

--- nvim/.config/nvim/lua/plugins/coding/align.lua
+++ nvim/.config/nvim/lua/plugins/coding/align.lua
@@ -1,15 +1,14 @@
+vim.api.nvim_create_user_command(
+  "Lma",
+  "Lazy load mini.align | echo 'mini.align loaded'",
+  { desc = "Load mini.align plugin" }
+)
+
 return {
   {
     "echasnovski/mini.align",
+    lazy = true,
     version = false,
-    keys = {
-      {
-        "lma",
-        "<cmd>Lazy load mini.align | echo 'mini.align loaded'<cr>",
-        mode = "n",
-        desc = "Load mini.align plugin",
-      },
-    },
     config = function(_, opts)
       require("mini.align").setup(opts)
     end,
