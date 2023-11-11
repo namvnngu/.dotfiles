35da58aa089e1c845042e28e0ec2fab1d7c77e26
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-11-12 00:18:08 +1100

    nvim: use prettier_d for js and ts when eslint not available

--- nvim/.config/nvim/lua/plugins/lang/javascript.lua
+++ nvim/.config/nvim/lua/plugins/lang/javascript.lua
@@ -7,4 +7,46 @@ return {
       end
     end,
   },
+
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      local eslint_config_files = {
+        ".eslintrc.js",
+        ".eslintrc.cjs",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.json",
+      }
+
+      opts.sources = opts.sources or {}
+
+      vim.list_extend(opts.sources, {
+        nls.builtins.code_actions.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(eslint_config_files)
+          end,
+        }),
+        nls.builtins.formatting.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(eslint_config_files)
+          end,
+        }),
+        nls.builtins.diagnostics.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(eslint_config_files)
+          end,
+        }),
+
+        nls.builtins.formatting.prettierd.with({
+          filetypes = { "javascript" },
+          condition = function(utils)
+            return not utils.root_has_file(eslint_config_files)
+          end,
+        }),
+      })
+    end,
+  },
 }
--- nvim/.config/nvim/lua/plugins/lang/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lang/typescript.lua
@@ -39,19 +39,37 @@ return {
     optional = true,
     opts = function(_, opts)
       local nls = require("null-ls")
+      local eslint_config_files = {
+        ".eslintrc.js",
+        ".eslintrc.cjs",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.json",
+      }
+
       opts.sources = opts.sources or {}
+
       vim.list_extend(opts.sources, {
-        nls.builtins.code_actions.eslint_d,
-        nls.builtins.formatting.eslint_d,
+        nls.builtins.code_actions.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(eslint_config_files)
+          end,
+        }),
+        nls.builtins.formatting.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(eslint_config_files)
+          end,
+        }),
         nls.builtins.diagnostics.eslint_d.with({
           condition = function(utils)
-            return utils.root_has_file({
-              ".eslintrc.js",
-              ".eslintrc.cjs",
-              ".eslintrc.yaml",
-              ".eslintrc.yml",
-              ".eslintrc.json",
-            })
+            return utils.root_has_file(eslint_config_files)
+          end,
+        }),
+
+        nls.builtins.formatting.prettierd.with({
+          filetypes = { "typescript" },
+          condition = function(utils)
+            return not utils.root_has_file(eslint_config_files)
           end,
         }),
       })
