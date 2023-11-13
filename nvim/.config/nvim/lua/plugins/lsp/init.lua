ef7fae4481269e01f25588529fc210db50c7d1df
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-11-14 00:04:03 +1100

    nvim: use latest version of fidget

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -79,10 +79,7 @@ return {
   -- Lsp progress
   {
     "j-hui/fidget.nvim",
-    tag = "legacy",
     event = "LspAttach",
-    config = function()
-      require("fidget").setup()
-    end,
+    opts = {},
   },
 }
