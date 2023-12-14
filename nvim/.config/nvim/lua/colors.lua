4012167115935b792bb191ec31377e9d1542ddcb
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-12-14 15:12:15 +1100

    nvim: delete colorscheme file since default colorscheme is used

---
+++ nvim/.config/nvim/lua/colors.lua
@@ -0,0 +1,12 @@
+-- Customize default theme
+
+-- Transparent Column
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "FoldColumn", { bg = "NONE", ctermbg = "NONE" })
+
+-- Transparent Background
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
