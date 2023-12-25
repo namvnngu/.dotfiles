9c8ab498bff4150a3b9a40ce9039c1504ce0a1fe
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-12-25 21:15:02 +1100

    nvim: try fzf-lua

---
+++ nvim/.config/nvim/lua/plugins/editor/fzf.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      -- git
+      {
+        "<C-p>",
+        "<cmd>FzfLua git_files debug=true<cr>",
+        mode = "n",
+        desc = "Search files via `git ls-files`",
+      },
+      -- files
+      {
+        "<leader>ff",
+        "<cmd>FzfLua files debug=true<cr>",
+        mode = "n",
+        desc = "Search files via `find` or `fd` on a path",
+      },
+      -- search
+      {
+        "<leader>fs",
+        "<cmd>FzfLua live_grep_glob debug=true<cr>",
+        mode = "n",
+        desc = "live_grep with rg --glob support",
+      },
+    },
+    opts = {
+      winopts = {
+        preview = {
+          hidden = "hidden",
+        },
+      },
+      grep = {
+        rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden --iglob=!.git -e",
+      },
+    },
+  },
+}
--- nvim/.config/nvim/lua/plugins/editor/telescope.lua
+++ nvim/.config/nvim/lua/plugins/editor/telescope.lua
@@ -22,92 +22,92 @@ return {
       require("telescope").load_extension("fzf")
     end,
     keys = {
-      -- git
-      {
-        "<C-p>",
-        "<cmd>Telescope git_files<cr>",
-        mode = "n",
-        desc = "Fuzzy search through the output of git ls-files command, respects .gitignore",
-      },
-      {
-        "<leader>fgs",
-        "<cmd>Telescope git_status<cr>",
-        mode = "n",
-        desc = "Lists current changes per file with diff preview and add action",
-      },
-      {
-        "<leader>fgb",
-        "<cmd>Telescope git_branches<cr>",
-        mode = "n",
-        desc = "Lists all branches with log preview",
-      },
-
-      -- files
-      {
-        "<leader>ff",
-        "<cmd>Telescope find_files<cr>",
-        mode = "n",
-        desc = "Lists files in your current working directory, respects .gitignore",
-      },
-      {
-        "<leader>flg",
-        "<cmd>Telescope live_grep<cr>",
-        mode = "n",
-        desc = "Searches for a string in your current working directory and get results live as you type, respects .gitignore.",
-      },
-      {
-        "<leader>fgw",
-        function()
-          require("telescope.builtin").grep_string({
-            search = vim.fn.expand("<cword>"),
-          })
-        end,
-        mode = "n",
-        desc = "Searches for the string under your cursor",
-      },
-      {
-        "<leader>fgs",
-        function()
-          local query = vim.trim(vim.fn.input("Grep For > "))
-          local root_dir = vim.trim(vim.fn.input("Root Dir > "))
-          require("telescope.builtin").grep_string({
-            cwd = #root_dir ~= 0 and root_dir
-              or require("telescope.utils").buffer_dir(),
-            search = query,
-            use_regex = true,
-          })
-        end,
-        mode = "n",
-        desc = "Searches for the string within a specific directory",
-      },
-
-      -- vim
-      {
-        "<leader>fbb",
-        "<cmd>Telescope buffers<cr>",
-        mode = "n",
-        desc = "Lists open buffers in current neovim instance",
-      },
-
-      -- lsp
-      {
-        "<leader>fgr",
-        "<cmd>Telescope lsp_references<cr>",
-        mode = "n",
-        desc = "Lists LSP references for word under the cursor",
-      },
-      {
-        "<leader>fgd",
-        "<cmd>Telescope lsp_definitions<cr>",
-        mode = "n",
-        desc = "Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope",
-      },
-      {
-        "<leader>fdo",
-        "<cmd>Telescope diagnostics<cr>",
-        mode = "n",
-        desc = "Lists Diagnostics for all open buffers or a specific buffer. Use option bufnr=0 for current buffer.",
-      },
+      -- -- git
+      -- {
+      --   "<C-p>",
+      --   "<cmd>Telescope git_files<cr>",
+      --   mode = "n",
+      --   desc = "Fuzzy search through the output of git ls-files command, respects .gitignore",
+      -- },
+      -- {
+      --   "<leader>fgs",
+      --   "<cmd>Telescope git_status<cr>",
+      --   mode = "n",
+      --   desc = "Lists current changes per file with diff preview and add action",
+      -- },
+      -- {
+      --   "<leader>fgb",
+      --   "<cmd>Telescope git_branches<cr>",
+      --   mode = "n",
+      --   desc = "Lists all branches with log preview",
+      -- },
+      --
+      -- -- files
+      -- {
+      --   "<leader>ff",
+      --   "<cmd>Telescope find_files<cr>",
+      --   mode = "n",
+      --   desc = "Lists files in your current working directory, respects .gitignore",
+      -- },
+      -- {
+      --   "<leader>flg",
+      --   "<cmd>Telescope live_grep<cr>",
+      --   mode = "n",
+      --   desc = "Searches for a string in your current working directory and get results live as you type, respects .gitignore.",
+      -- },
+      -- {
+      --   "<leader>fgw",
+      --   function()
+      --     require("telescope.builtin").grep_string({
+      --       search = vim.fn.expand("<cword>"),
+      --     })
+      --   end,
+      --   mode = "n",
+      --   desc = "Searches for the string under your cursor",
+      -- },
+      -- {
+      --   "<leader>fgs",
+      --   function()
+      --     local query = vim.trim(vim.fn.input("Grep For > "))
+      --     local root_dir = vim.trim(vim.fn.input("Root Dir > "))
+      --     require("telescope.builtin").grep_string({
+      --       cwd = #root_dir ~= 0 and root_dir
+      --         or require("telescope.utils").buffer_dir(),
+      --       search = query,
+      --       use_regex = true,
+      --     })
+      --   end,
+      --   mode = "n",
+      --   desc = "Searches for the string within a specific directory",
+      -- },
+      --
+      -- -- vim
+      -- {
+      --   "<leader>fbb",
+      --   "<cmd>Telescope buffers<cr>",
+      --   mode = "n",
+      --   desc = "Lists open buffers in current neovim instance",
+      -- },
+      --
+      -- -- lsp
+      -- {
+      --   "<leader>fgr",
+      --   "<cmd>Telescope lsp_references<cr>",
+      --   mode = "n",
+      --   desc = "Lists LSP references for word under the cursor",
+      -- },
+      -- {
+      --   "<leader>fgd",
+      --   "<cmd>Telescope lsp_definitions<cr>",
+      --   mode = "n",
+      --   desc = "Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope",
+      -- },
+      -- {
+      --   "<leader>fdo",
+      --   "<cmd>Telescope diagnostics<cr>",
+      --   mode = "n",
+      --   desc = "Lists Diagnostics for all open buffers or a specific buffer. Use option bufnr=0 for current buffer.",
+      -- },
     },
   },
 
