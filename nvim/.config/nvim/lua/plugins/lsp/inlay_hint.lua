264617bbb5b2d9426513decc2250d4d8c1c30811
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-12-26 15:50:01 +1100

    nvim: update inlay_hint

--- nvim/.config/nvim/lua/plugins/lsp/inlay_hint.lua
+++ nvim/.config/nvim/lua/plugins/lsp/inlay_hint.lua
@@ -1,9 +1,13 @@
 local M = {}
 
 function M.on_attach(client, buffer)
- local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+  local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
   if client.server_capabilities.inlayHintProvider and inlay_hint then
-    inlay_hint(buffer, true)
+    if type(inlay_hint) == "function" then
+      inlay_hint(buffer, true)
+    elseif type(inlay_hint) == "table" and inlay_hint.enable then
+      inlay_hint.enable(buffer, true)
+    end
   end
 end
 
