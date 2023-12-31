f79632586d77f3b3136472845c357e3bebaced86
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-12-31 17:58:57 +1100

    nvim: update opts of markdown-preview.nvim

--- nvim/.config/nvim/lua/plugins/lang/markdown.lua
+++ nvim/.config/nvim/lua/plugins/lang/markdown.lua
@@ -1,10 +1,10 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_preview_options = { disable_sync_scroll = false }
     end,
     ft = { "markdown" },
   },
