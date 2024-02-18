8e5aeb8d455376b97a804ec612b3218065ec57fd
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-02-19 00:12:16 +1100

    nvim: disable treesitter on large files

--- nvim/.config/nvim/lua/plugins/lang/css.lua
+++ nvim/.config/nvim/lua/plugins/lang/css.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    optional = true,
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "css", "scss" })
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -22,6 +22,10 @@ return {
       highlight = {
         enable = true,
 
+        disable = function(_, bufnr)
+          return vim.api.nvim_buf_line_count(bufnr) >= 1000
+        end,
+
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
