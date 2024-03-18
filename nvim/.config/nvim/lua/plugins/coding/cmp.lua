cecd3bcc1d04c2ffb0da394aa674ac43a16d15df
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-03-18 14:24:34 +1100

    nvim: remove tab keymapping for cmp

--- nvim/.config/nvim/lua/plugins/coding/cmp.lua
+++ nvim/.config/nvim/lua/plugins/coding/cmp.lua
@@ -24,21 +24,7 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end,
-          ["<S-TAB>"] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end,
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
