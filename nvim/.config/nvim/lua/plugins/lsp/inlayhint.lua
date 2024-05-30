de655a998838813e07e2fe1fb59af80a7a3a7486
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-05-30 12:19:27 +1000

    nvim: refactor on_attach for inlay hint

--- nvim/.config/nvim/lua/plugins/lsp/inlayhint.lua
+++ nvim/.config/nvim/lua/plugins/lsp/inlayhint.lua
@@ -1,13 +1,8 @@
 local M = {}
 
-function M.on_attach(client, buffer)
-  local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
-  if client.server_capabilities.inlayHintProvider and inlay_hint then
-    if type(inlay_hint) == "function" then
-      inlay_hint(buffer, true)
-    elseif type(inlay_hint) == "table" and inlay_hint.enable then
-      inlay_hint.enable(buffer, true)
-    end
+function M.on_attach(client, bufnr)
+  if client.server_capabilities.inlayHintProvider then
+    vim.lsp.inlay_hint.enable(true, { bufnr })
   end
 end
 
