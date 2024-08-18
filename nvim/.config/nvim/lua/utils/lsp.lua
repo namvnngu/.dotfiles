6af75ab7af2a5579ca2bb3cdcbb2558d79d922fb
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-08-18 16:21:28 +1000

    nvim: improve lsp.on_attach util

--- nvim/.config/nvim/lua/utils/lsp.lua
+++ nvim/.config/nvim/lua/utils/lsp.lua
@@ -1,12 +1,13 @@
 local M = {}
 
----@param on_attach fun(client :vim.lsp.Client, bufnr :number)
-function M.on_attach(on_attach)
+---@param on_attach fun(client:vim.lsp.Client, buffer)
+---@param name? string
+function M.on_attach(on_attach, name)
   return vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
       local bufnr = args.buf ---@type number
       local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if client then
+      if client and (not name or client.name == name) then
         return on_attach(client, bufnr)
       end
     end,
