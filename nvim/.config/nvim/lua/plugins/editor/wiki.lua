020ca21c5300cb41cd9dcd50734dec31f32567d2
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-08-18 16:43:14 +1000

    nvim+vim: fall back to ~/wiki when there is no WIKI_ROOT_PATH env defined

--- nvim/.config/nvim/lua/plugins/editor/wiki.lua
+++ nvim/.config/nvim/lua/plugins/editor/wiki.lua
@@ -2,17 +2,14 @@ return {
   {
     "lervag/wiki.vim",
     init = function()
+      vim.g.wiki_root = os.getenv("WIKI_ROOT_PATH") or "~/wiki"
+      if vim.fn.isdirectory(vim.g.wiki_root) == 0 then
+        require("utils.system").run_cmd("mkdir " .. vim.g.wiki_root)
+      end
+
       vim.keymap.set("n", "<leader>wx", "<plug>(wiki-pages)", {
         desc = "List wiki files in current wiki or in the main wiki defined by |g:wiki_root|",
       })
-
-      local wiki_root_path_env = os.getenv("WIKI_ROOT_PATH")
-      if wiki_root_path_env ~= nil then
-        vim.g.wiki_root = wiki_root_path_env
-        if vim.fn.isdirectory(vim.g.wiki_root) == 0 then
-          require("utils.system").run_cmd("mkdir " .. vim.g.wiki_root)
-        end
-      end
     end,
   },
 }
--- vim/.vimrc
+++ vim/.vimrc
@@ -232,10 +232,8 @@ call s:download_plugin("https://github.com/tpope/vim-fugitive", "vim-fugitive")
 
 call s:download_plugin("https://github.com/lervag/wiki.vim", "wiki.vim")
 let s:wiki_root_path_env = getenv("WIKI_ROOT_PATH")
-if !empty(s:wiki_root_path_env)
-  let g:wiki_root = s:wiki_root_path_env
-  if !isdirectory(g:wiki_root)
-    call system("mkdir " . g:wiki_root)
-  endif
-  nnoremap <leader>wx <plug>(wiki-pages)
+let g:wiki_root = empty(s:wiki_root_path_env) ? "~/wiki" : s:wiki_root_path_env
+if !isdirectory(g:wiki_root)
+  call system("mkdir " . g:wiki_root)
 endif
+nnoremap <leader>wx <plug>(wiki-pages)
