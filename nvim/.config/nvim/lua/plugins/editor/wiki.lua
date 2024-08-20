bcab1d5d704e3c6796d90804ceb06da0046ee277
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-08-21 00:57:28 +1000

    nvim: fix wiki.vim not able to create root folder

--- nvim/.config/nvim/lua/plugins/editor/wiki.lua
+++ nvim/.config/nvim/lua/plugins/editor/wiki.lua
@@ -3,7 +3,7 @@ return {
     "lervag/wiki.vim",
     init = function()
       vim.g.wiki_root =
-        vim.fn.expand(vim.fn.getenv("WIKI_ROOT_PATH") or "~/wiki")
+        vim.fn.expand(os.getenv("WIKI_ROOT_PATH") or "~/wiki")
       if vim.fn.isdirectory(vim.g.wiki_root) == 0 then
         require("utils.system").run_cmd("mkdir " .. vim.g.wiki_root)
       end
