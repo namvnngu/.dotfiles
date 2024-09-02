7139502984ef9f9e78cee89e5ee90d3da94f8664
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-09-02 18:53:28 +1000

    nvim: format float lsp diagnostic messages

--- nvim/.config/nvim/lua/plugins/lsp/diagnostic.lua
+++ nvim/.config/nvim/lua/plugins/lsp/diagnostic.lua
@@ -1,33 +1,34 @@
 local M = {}
 
+local SEVERITY_LABELS = {
+  [vim.diagnostic.severity.INFO] = "I",
+  [vim.diagnostic.severity.HINT] = "H",
+  [vim.diagnostic.severity.WARN] = "W",
+  [vim.diagnostic.severity.ERROR] = "E",
+}
+
+--- @param diagnostic vim.Diagnostic The diagnostic
+--- @return string diagnostic_message A diagnostic message
+local function format_diagnostic_message(diagnostic)
+  local severity_label = SEVERITY_LABELS[diagnostic.severity]
+
+  if not severity_label then
+    require("utils.log").run(
+      string.format("Unknown diagnostic severity, %s.", diagnostic.severity)
+    )
+  end
+
+  return string.format(
+    "[%s] %s",
+    severity_label or "UNKNOWN",
+    diagnostic.message
+  )
+end
+
 function M.on_attach()
-  local SEVERITY_LABELS = {
-    [vim.diagnostic.severity.INFO] = "I",
-    [vim.diagnostic.severity.HINT] = "H",
-    [vim.diagnostic.severity.WARN] = "W",
-    [vim.diagnostic.severity.ERROR] = "E",
-  }
   vim.diagnostic.config({
-    virtual_text = {
-      format = function(diagnostic)
-        local severity_label = SEVERITY_LABELS[diagnostic.severity]
-
-        if not severity_label then
-          require("utils.log").run(
-            string.format(
-              "Unknown diagnostic severity, %s.",
-              diagnostic.severity
-            )
-          )
-        end
-
-        return string.format(
-          "[%s] %s",
-          severity_label or "UNKNOWN",
-          diagnostic.message
-        )
-      end,
-    },
+    float = { format = format_diagnostic_message },
+    virtual_text = { format = format_diagnostic_message },
   })
 end
 
