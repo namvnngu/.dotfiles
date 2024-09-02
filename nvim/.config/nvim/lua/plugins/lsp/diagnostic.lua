f7c04182c12097a08d5bf8a32fa9d0caf166c366
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-09-02 18:31:31 +1000

    nvim: improve lsp diagnostic messages

--- nvim/.config/nvim/lua/plugins/lsp/diagnostic.lua
+++ nvim/.config/nvim/lua/plugins/lsp/diagnostic.lua
@@ -1,7 +1,23 @@
 local M = {}
 
 function M.on_attach()
-  vim.diagnostic.config({})
+  vim.diagnostic.config({
+    virtual_text = {
+      format = function(diagnostic)
+        local severity_labels = {
+          [vim.diagnostic.severity.INFO] = "I",
+          [vim.diagnostic.severity.HINT] = "H",
+          [vim.diagnostic.severity.WARN] = "W",
+          [vim.diagnostic.severity.ERROR] = "E",
+        }
+        return string.format(
+          "[%s] %s",
+          severity_labels[diagnostic.severity] or "unknown",
+          diagnostic.message
+        )
+      end,
+    },
+  })
 end
 
 return M
