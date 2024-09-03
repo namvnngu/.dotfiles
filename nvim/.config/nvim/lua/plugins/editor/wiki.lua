41c8c13252fec87fe28e6e3f11ff4b43e7307001
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-09-04 09:31:23 +1000

    nvim+vim: remove unused wiki commands

--- nvim/.config/nvim/lua/plugins/editor/wiki.lua
+++ nvim/.config/nvim/lua/plugins/editor/wiki.lua
@@ -8,35 +8,8 @@ return {
         require("utils.system").run_cmd("mkdir " .. vim.g.wiki_root)
       end
 
-      -- keymaps
       vim.keymap.set("n", "<leader>wx", "<plug>(wiki-pages)", {
         desc = "List wiki files in current wiki or in the main wiki defined by |g:wiki_root|",
-      })
-
-      -- commands
-      vim.api.nvim_create_user_command("Wjp", function()
-        local filetype = vim.bo.filetype
-        if filetype == "markdown" then
-          vim.cmd("WikiJournalPrev")
-        else
-          require("utils.notify").error(
-            "Wjp is not available outside of markdown files"
-          )
-        end
-      end, {
-        desc = "Go to the previous journal entry (at current level of daily, weekly, monthly)",
-      })
-      vim.api.nvim_create_user_command("Wjn", function()
-        local filetype = vim.bo.filetype
-        if filetype == "markdown" then
-          vim.cmd("WikiJournalNext")
-        else
-          require("utils.notify").error(
-            "Wjn is not available outside of markdown files"
-          )
-        end
-      end, {
-        desc = "Go to the next journal entry (at current level of daily, weekly, monthly)",
       })
     end,
   },
