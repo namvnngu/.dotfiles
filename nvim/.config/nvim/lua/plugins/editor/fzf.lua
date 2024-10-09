33039dcf7f90b7028132ec7f9a3c030046e9e230
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-10-10 01:28:26 +1100

    nvim+vim: use fzf from ~/.fzf instead of fzf-lua and fzf.vim

--- nvim/.config/nvim/lua/plugins/editor/fzf.lua
+++ nvim/.config/nvim/lua/plugins/editor/fzf.lua
@@ -1,50 +1,12 @@
 return {
   {
-    "ibhagwan/fzf-lua",
+    dir = "~/.fzf",
     keys = {
-      -- git
       {
         "<C-p>",
-        "<Cmd>FzfLua git_files<CR>",
+        "<Cmd>FZF<CR>",
         mode = "n",
-        desc = "Search files via `git ls-files`",
-      },
-      -- buffers and files
-      {
-        "<leader>ff",
-        "<Cmd>FzfLua files<CR>",
-        mode = "n",
-        desc = "Search files via `find` or `fd` on a path",
-      },
-      {
-        "<leader>fb",
-        "<Cmd>FzfLua buffers<CR>",
-        mode = "n",
-        desc = "Open buffers",
-      },
-      -- search
-      {
-        "<leader>fs",
-        "<Cmd>FzfLua live_grep_glob<CR>",
-        mode = "n",
-        desc = "live_grep with rg --glob support",
-      },
-      -- misc
-      {
-        "<leader>fr",
-        "<Cmd>FzfLua resume<CR>",
-        mode = "n",
-        desc = "Resume last command/query",
-      },
-    },
-    opts = {
-      winopts = {
-        preview = {
-          hidden = "hidden",
-        },
-      },
-      grep = {
-        rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden --iglob=!.git -e",
+        desc = "Fuzzy Finder",
       },
     },
   },
