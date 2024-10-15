d12db8005693093b28b893e37e78af2a82ad0a01
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-10-15 11:58:31 +1100

    nvim: move plugman.add to plugman.lua

--- nvim-next/.config/nvim/lua/plugins.lua
+++ nvim-next/.config/nvim/lua/plugins.lua
@@ -1,9 +1,1 @@
-local M = {}
 
---- Add plugin
---- @param name string Plugin name.
-function M.add(name)
-  print(name)
-end
-
-return M
---
+++ nvim-next/.config/nvim/lua/utils/plugman.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+--- Add plugin
+--- @param name string Plugin name.
+function M.add(name)
+  print(name)
+end
+
+return M
