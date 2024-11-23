370e3a1815c080437594ab02c27f82d643f1fb0f
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-11-24 01:11:30 +1100

    nvim: update lsp keymaps

--- nvim/.config/nvim/lua/plugins/lsp/keymaps.lua
+++ nvim/.config/nvim/lua/plugins/lsp/keymaps.lua
@@ -7,56 +7,43 @@ function M.on_attach()
 
   --- Reference: https://github.com/neovim/neovim/blob/master/runtime/lua/vim/_defaults.lua#L154
   do
-    vim.keymap.set(
-      "n",
-      "grn",
-      vim.lsp.buf.rename,
-      { desc = "vim.lsp.buf.rename()" }
-    )
+    vim.keymap.set("n", "grn", function()
+      vim.lsp.buf.rename()
+    end, { desc = "vim.lsp.buf.rename()" })
 
-    vim.keymap.set(
-      { "n", "x" },
-      "gra",
-      vim.lsp.buf.code_action,
-      { desc = "vim.lsp.buf.code_action()" }
-    )
+    vim.keymap.set({ "n", "x" }, "gra", function()
+      vim.lsp.buf.code_action()
+    end, { desc = "vim.lsp.buf.code_action()" })
 
-    vim.keymap.set(
-      "n",
-      "grr",
-      vim.lsp.buf.references,
-      { desc = "vim.lsp.buf.references()" }
-    )
+    vim.keymap.set("n", "grr", function()
+      vim.lsp.buf.references()
+    end, { desc = "vim.lsp.buf.references()" })
 
-    vim.keymap.set(
-      "i",
-      "<C-S>",
-      vim.lsp.buf.signature_help,
-      { desc = "vim.lsp.buf.signature_help()" }
-    )
+    vim.keymap.set("n", "gri", function()
+      vim.lsp.buf.implementation()
+    end, { desc = "vim.lsp.buf.implementation()" })
+
+    vim.keymap.set("n", "gO", function()
+      vim.lsp.buf.document_symbol()
+    end, { desc = "vim.lsp.buf.document_symbol()" })
+
+    vim.keymap.set({ "i", "s" }, "<C-S>", function()
+      vim.lsp.buf.signature_help()
+    end, { desc = "vim.lsp.buf.signature_help()" })
   end
 
   do
-    vim.keymap.set(
-      "n",
-      "]d",
-      vim.diagnostic.goto_next,
-      { desc = "Jump to the next diagnostic in the current buffer" }
-    )
+    vim.keymap.set("n", "]d", function()
+      vim.diagnostic.goto_next()
+    end, { desc = "Jump to the next diagnostic in the current buffer" })
 
-    vim.keymap.set(
-      "n",
-      "[d",
-      vim.diagnostic.goto_prev,
-      { desc = "Jump to the previous diagnostic in the current buffer" }
-    )
+    vim.keymap.set("n", "[d", function()
+      vim.diagnostic.goto_prev()
+    end, { desc = "Jump to the previous diagnostic in the current buffer" })
 
-    vim.keymap.set(
-      "n",
-      "<C-W>d",
-      vim.diagnostic.open_float,
-      { desc = "Show diagnostics under the cursor" }
-    )
+    vim.keymap.set("n", "<C-W>d", function()
+      vim.diagnostic.open_float()
+    end, { desc = "Show diagnostics under the cursor" })
 
     vim.keymap.set(
       "n",
