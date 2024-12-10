3b1361330c79dbfd939d46f315507c0e5969db87
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-12-10 11:33:50 +1100

    nvim: improve lazy config

--- nvim/.config/nvim/lua/lazynvim.lua
+++ nvim/.config/nvim/lua/lazynvim.lua
@@ -24,32 +24,12 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { import = "plugins" },
+    { import = "plugins.lang" },
     { import = "plugins.coding" },
     { import = "plugins.editor" },
-    { import = "plugins.lang" },
   },
-  defaults = {
-    lazy = false, -- should plugins be lazy-loaded?
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = {} },
-  checker = { enabled = false }, -- automatically check for plugin updates
-  change_detection = { enabled = false }, -- automatically check for config file changes and reload the ui
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        -- "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
+  change_detection = {
+    enabled = false,
   },
   ui = {
     icons = {
