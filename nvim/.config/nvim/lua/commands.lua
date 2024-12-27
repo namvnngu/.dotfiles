2ded84d165636109ca955d204973387a6263fc2a
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-12-28 01:18:30 +1100

    nvim: improve LspCapabilities

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -3,25 +3,21 @@ local notify = require("utils.notify")
 
 --- List all capabilities of the server associated with the current buffer
 vim.api.nvim_create_user_command("LspCapabilities", function()
-  local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  local clients =
+    vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
 
   for _, client in pairs(clients) do
-    if client.name ~= "null-ls" then
-      local capabilities = {}
-      for capability_name, is_active in pairs(client.server_capabilities) do
-        if is_active and capability_name:find("Provider") then
-          table.insert(capabilities, "- " .. capability_name)
-        end
+    local capabilities = {}
+    for capability_name, is_active in pairs(client.server_capabilities) do
+      if is_active and capability_name:find("Provider") then
+        table.insert(capabilities, "- " .. capability_name)
       end
-
-      table.sort(capabilities)
-
-      local msg = "# "
-        .. client.name
-        .. "\n"
-        .. table.concat(capabilities, "\n")
-      notify.trace(msg)
     end
+
+    table.sort(capabilities)
+
+    local msg = "# " .. client.name .. "\n" .. table.concat(capabilities, "\n")
+    notify.trace(msg)
   end
 end, {})
 
