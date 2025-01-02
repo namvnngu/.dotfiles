b12a36f51e3443fb561903b78590989f652a6517
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-01-02 15:20:18 +1100

    vim+nvim: update theme

--- nvim/.config/nvim/lua/plugins/editor/theme.lua
+++ nvim/.config/nvim/lua/plugins/editor/theme.lua
@@ -1,11 +1,19 @@
 vim.cmd("colorscheme quiet")
 if vim.o.background == "light" then
   vim.api.nvim_set_hl(0, "Comment", { fg = "#949494", ctermfg = 246 })
-  vim.api.nvim_set_hl(0, "Normal", { fg = "#212427", bg = "white", ctermfg = "black", ctermbg = "white" })
+  vim.api.nvim_set_hl(
+    0,
+    "Normal",
+    { fg = "#242424", bg = "#FFFFFF", ctermfg = "black", ctermbg = "white" }
+  )
   vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 else
   vim.api.nvim_set_hl(0, "Comment", { fg = "#585858", ctermfg = 240 })
-  vim.api.nvim_set_hl(0, "Normal", { fg = "white", bg = "black", ctermfg = "white", ctermbg = "black" })
+  vim.api.nvim_set_hl(
+    0,
+    "Normal",
+    { fg = "#F3F3F3", bg = "#121212", ctermfg = "white", ctermbg = "black" }
+  )
   vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 end
 
