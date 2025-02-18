388f9994d5ae1c40b95be1cde69dc5e7dc08a948
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-02-18 22:46:14 +1100

    nvim: improve plugin installing with non-blocking

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -15,35 +15,52 @@ local PLUG_ROOT = vim.fn.stdpath("data") .. "/site/pack/plugins/start"
 --- @param plug_urls string[] An array of plugin urls.
 --- @param plug_root string A root path where plugins will be installed.
 local function install(plug_urls, plug_root)
+  local installing_count = 0
+
   for _, plug_url in pairs(plug_urls) do
     local plug_name = vim.fn.fnamemodify(plug_url, ":t")
     local plug_path = vim.fn.expand(plug_root .. "/" .. plug_name)
 
     if vim.fn.isdirectory(plug_path) == 0 then
+      installing_count = installing_count + 1
+
       vim.api.nvim_echo({
         { "Installing " .. plug_name .. "..." },
       }, true, {})
 
-      local is_ok, output = pcall(vim.fn.system, {
+      vim.fn.jobstart({
         "git",
         "clone",
         "--depth=1",
         "--filter=blob:none",
         plug_url,
         plug_path,
-      })
+      }, {
+        on_exit = function(_, exit_code, _)
+          installing_count = installing_count - 1
 
-      if is_ok and vim.v.shell_error == 0 then
-        vim.api.nvim_echo({
-          { "Installed " .. plug_name .. "!\n\n" },
-        }, true, {})
-      else
-        vim.api.nvim_echo({
-          { "Failed to install " .. plug_name .. "\n", "ErrorMsg" },
-          { vim.trim(output or "") .. "\n\n", "WarningMsg" },
-        }, true, {})
-      end
+          if exit_code == 0 then
+            vim.api.nvim_echo({
+              { "Installed " .. plug_name .. "!" },
+            }, true, {})
+          else
+            vim.api.nvim_echo({
+              {
+                "Failed to install "
+                  .. plug_name
+                  .. " with exit code "
+                  .. exit_code,
+                "ErrorMsg",
+              },
+            }, true, {})
+          end
+        end,
+      })
     end
+  end
+
+  while installing_count ~= 0 do
+    vim.wait(10)
   end
 end
 
@@ -62,22 +79,19 @@ local function create_commands(plug_urls, plug_root)
   end, { desc = "Update plugins" })
 
   vim.api.nvim_create_user_command("Pc", function()
-    vim.api.nvim_echo({
-      { "Plugin count: " .. #plug_urls },
-    }, true, {})
+    vim.api.nvim_echo({ { "Plugin count: " .. #plug_urls } }, true, {})
   end, { desc = "Count plugins" })
 
   vim.api.nvim_create_user_command("Pl", function()
-    local message = ""
+    local msg = ""
     for index, plug_url in pairs(plug_urls) do
       local plug_name = vim.fn.fnamemodify(plug_url, ":t")
-      message =
-        string.format("%s%s. %s: %s\n", message, index, plug_name, plug_url)
+      msg = string.format("%s%s. %s: %s\n", msg, index, plug_name, plug_url)
     end
 
-    vim.api.nvim_echo({
-      { message },
-    }, true, {})
+    if msg ~= "" then
+      vim.api.nvim_echo({ { msg } }, true, {})
+    end
   end, { desc = "List plugins" })
 end
 
