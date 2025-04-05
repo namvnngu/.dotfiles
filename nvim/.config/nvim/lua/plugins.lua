c1b2ec0aa3f62b55d4c27dded8650a865aeb5f21
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-04-06 01:39:46 +1100

    nvim: rename variables in plugins.lua

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
-local PLUG_URLS = {
+local PLUGIN_URLS = {
   -- Required
   "https://github.com/junegunn/fzf",
   "https://github.com/stevearc/oil.nvim",
@@ -12,13 +12,13 @@ local PLUG_URLS = {
   "https://github.com/echasnovski/mini.align",
   "https://github.com/Decodetalkers/csharpls-extended-lsp.nvim",
 }
-local PLUG_ROOT = vim.fn.stdpath("data") .. "/site/pack/plugins/start"
+local PLUGIN_ROOT = vim.fn.stdpath("data") .. "/site/pack/plugins/start"
 
 --- Gets the plugin name from a given URL or path.
 ---
 --- @param urlOrPath string The URL or file path of the plugin.
 --- @return string The plugin name.
-local function get_plug_name(urlOrPath)
+local function get_plugin_name(urlOrPath)
   return vim.fn.fnamemodify(urlOrPath, ":t")
 end
 
@@ -32,41 +32,39 @@ end
 
 --- Installs a list of plugins given their URLS.
 ---
---- @param plug_urls string[] List of plugin URLs to be installed.
---- @param plug_root string The root directory where plugins are stored.
-local function install_plugs(plug_urls, plug_root)
-  if vim.tbl_isempty(plug_urls) then
+--- @param plugin_urls string[] List of plugin URLs to be installed.
+--- @param plugin_root string The root directory where plugins are stored.
+local function install_plugs(plugin_urls, plugin_root)
+  if vim.tbl_isempty(plugin_urls) then
     return
   end
 
   local job_ids = {}
 
-  for _, plug_url in pairs(plug_urls) do
-    local plug_name = get_plug_name(plug_url)
-    local plug_path = vim.fn.expand(("%s/%s"):format(plug_root, plug_name))
+  for _, plugin_url in pairs(plugin_urls) do
+    local name = get_plugin_name(plugin_url)
+    local path = vim.fn.expand(("%s/%s"):format(plugin_root, name))
 
-    if vim.fn.isdirectory(plug_path) == 0 then
-      echo(("Installing %s..."):format(plug_name))
+    if vim.fn.isdirectory(path) == 0 then
+      echo(("Installing %s..."):format(name))
 
       local job_id = vim.fn.jobstart({
         "git",
         "clone",
         "--depth=1",
         "--filter=blob:none",
-        plug_url,
-        plug_path,
+        plugin_url,
+        path,
       }, {
         on_exit = function(_, exit_code, _)
           if exit_code == 0 then
-            echo(("Installed %s!"):format(plug_name))
+            echo(("Installed %s!"):format(name))
           else
-            echo(
-              ("Failed to install %s with exit code %s"):format(
-                plug_name,
-                exit_code
-              ),
-              true
+            local err_msg = ("Failed to install %s with exit code %s"):format(
+              name,
+              exit_code
             )
+            echo(err_msg, true)
           end
         end,
       })
@@ -84,47 +82,47 @@ end
 --- desired plugin URLs. It removes any plugins that are no longer needed and
 --- installs any new ones that are missing.
 ---
---- @param next_plug_urls string[] List of desired plugin URLs.
---- @param plug_root string The root directory where plugins are stored.
+--- @param next_plugin_urls string[] List of desired plugin URLs.
+--- @param plugin_root string The root directory where plugins are stored.
 --- @return boolean `true` if no changes are needed (already synced).
-local function sync_plugs(next_plug_urls, plug_root)
-  local function build_plug_name_map(list)
+local function sync_plugins(next_plugin_urls, plugin_root)
+  local function build_map(list)
     return vim.iter(list):fold({}, function(acc, item)
-      acc[get_plug_name(item)] = item
+      acc[get_plugin_name(item)] = item
       return acc
     end)
   end
 
-  local current_plug_paths = vim.fn.globpath(plug_root, "*", false, true)
-  local current_plug_path_by_name = build_plug_name_map(current_plug_paths)
-  local next_plug_url_by_name = build_plug_name_map(next_plug_urls)
+  local current_plugin_paths = vim.fn.globpath(plugin_root, "*", false, true)
+  local current_plugin_path_by_name = build_map(current_plugin_paths)
+  local next_plugin_url_by_name = build_map(next_plugin_urls)
 
-  local removed_plug_path_by_name = {}
-  for plug_name, plug_path in pairs(current_plug_path_by_name) do
-    if not next_plug_url_by_name[plug_name] then
-      removed_plug_path_by_name[plug_name] = plug_path
+  local removed_plugin_path_by_name = {}
+  for name, path in pairs(current_plugin_path_by_name) do
+    if not next_plugin_url_by_name[name] then
+      removed_plugin_path_by_name[name] = path
     end
   end
-  if not vim.tbl_isempty(removed_plug_path_by_name) then
-    local names = vim.tbl_keys(removed_plug_path_by_name)
-    local paths = vim.tbl_values(removed_plug_path_by_name)
+  if not vim.tbl_isempty(removed_plugin_path_by_name) then
+    local names = vim.tbl_keys(removed_plugin_path_by_name)
+    local paths = vim.tbl_values(removed_plugin_path_by_name)
     echo(("Removing %s..."):format(vim.fn.join(names, "...\nRemoving ")))
     vim.fn.system(vim.list_extend({ "rm", "-rf" }, paths))
     echo(("Removed %s!"):format(vim.fn.join(names, "!\nRemoved ")))
   end
 
-  local added_plug_urls = {}
-  for plug_name, plug_url in pairs(next_plug_url_by_name) do
-    if not current_plug_path_by_name[plug_name] then
-      table.insert(added_plug_urls, plug_url)
+  local added_plugin_urls = {}
+  for name, url in pairs(next_plugin_url_by_name) do
+    if not current_plugin_path_by_name[name] then
+      table.insert(added_plugin_urls, url)
     end
   end
-  if not vim.tbl_isempty(added_plug_urls) then
-    install_plugs(added_plug_urls, plug_root)
+  if not vim.tbl_isempty(added_plugin_urls) then
+    install_plugs(added_plugin_urls, plugin_root)
   end
 
-  local no_changes = vim.tbl_isempty(removed_plug_path_by_name)
-    and vim.tbl_isempty(added_plug_urls)
+  local no_changes = vim.tbl_isempty(removed_plugin_path_by_name)
+    and vim.tbl_isempty(added_plugin_urls)
 
   if not no_changes then
     echo("Restart Nvim to get latest updates.")
@@ -135,35 +133,35 @@ end
 
 --- Creates plugin commands.
 ---
---- @param plug_urls string[] List of plugin URLs to be installed.
---- @param plug_root string The root directory where plugins are stored.
-local function create_cmds(plug_urls, plug_root)
+--- @param next_plugin_urls string[] List of desired plugin URLs.
+--- @param plugin_root string The root directory where plugins are stored.
+local function create_commands(next_plugin_urls, plugin_root)
   vim.api.nvim_create_user_command("Pu", function()
-    vim.fn.system({ "rm", "-rf", plug_root })
-    install_plugs(plug_urls, plug_root)
+    vim.fn.system({ "rm", "-rf", plugin_root })
+    install_plugs(next_plugin_urls, plugin_root)
     echo("Updated all plugins. Restart Nvim to get latest updates.")
   end, { desc = "Update all plugins" })
 
   vim.api.nvim_create_user_command("Ps", function()
-    local no_changes = sync_plugs(plug_urls, plug_root)
+    local no_changes = sync_plugins(next_plugin_urls, plugin_root)
     if no_changes then
       echo("All plugins are already synced.")
     end
   end, { desc = "Sync plugins" })
 
   vim.api.nvim_create_user_command("Pc", function()
-    local plug_paths = vim.fn.globpath(plug_root, "*", false, true)
-    echo("Plugin count: " .. vim.tbl_count(plug_paths))
+    local paths = vim.fn.globpath(plugin_root, "*", false, true)
+    echo("Plugin count: " .. vim.tbl_count(paths))
   end, { desc = "Count plugins" })
 
   vim.api.nvim_create_user_command("Pl", function()
-    local plug_paths = vim.fn.globpath(plug_root, "*", false, true)
-    for index, path in pairs(plug_paths) do
-      local name = get_plug_name(path)
+    local paths = vim.fn.globpath(plugin_root, "*", false, true)
+    for index, path in pairs(paths) do
+      local name = get_plugin_name(path)
       echo(("%s. %s"):format(index, name))
     end
   end, { desc = "List plugins" })
 end
 
-sync_plugs(PLUG_URLS, PLUG_ROOT)
-create_cmds(PLUG_URLS, PLUG_ROOT)
+sync_plugins(PLUGIN_URLS, PLUGIN_ROOT)
+create_commands(PLUGIN_URLS, PLUGIN_ROOT)
