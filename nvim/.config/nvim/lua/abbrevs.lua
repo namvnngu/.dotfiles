513025738141416631d5f7e46b9b136cefd8c9f6
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-04-25 19:59:31 +1000

    vim+nvim: update me:: abbrev

--- nvim/.config/nvim/lua/abbrevs.lua
+++ nvim/.config/nvim/lua/abbrevs.lua
@@ -2,6 +2,10 @@ vim.keymap.set(
   "ia",
   "me::",
   vim.fn.strftime(
-    "Author: Nam Nguyen <vnngucs@outlook.com><CR>Date: %d %b %Y<CR>License: The Unlicense"
+    "Author: Nam Nguyen <vnngucs@outlook.com><CR>"
+      .. "Description:<CR>"
+      .. "License: The Unlicense<CR>"
+      .. "Date created: %B %d %Y<CR>"
+      .. "Date modified: %B %d %Y"
   )
 )
