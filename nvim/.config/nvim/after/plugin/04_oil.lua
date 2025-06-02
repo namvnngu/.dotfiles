287a81cc6b84871376891189b2a620c0654510f8
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-06-02 20:58:41 +1000

    nvim: hide columns in oil

--- nvim/.config/nvim/after/plugin/04_oil.lua
+++ nvim/.config/nvim/after/plugin/04_oil.lua
@@ -3,12 +3,6 @@ require("oil").setup({
   view_options = {
     show_hidden = true,
   },
-  columns = {
-    "permissions",
-    "size",
-    "mtime",
-    "birthtime",
-  },
 })
 
 vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
