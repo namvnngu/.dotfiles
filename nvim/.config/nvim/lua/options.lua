9b4201139033d2a2fd9c7f69f535f01aaf6cc54d
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-06-04 15:52:14 +1000

    vim+nvim: remove note comment

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.cmd("syntax off") -- Disabled by default to prevent nvim from hanging on large files
+vim.cmd("syntax off")
 vim.cmd("filetype plugin indent on")
 vim.cmd("colorscheme wildcharm")
 
