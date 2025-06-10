1131f9a2d5e98d3885d0769ac554adc546f074eb
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-06-10 23:16:30 +1000

    nvim: change cc to c++ for cpp runner

--- nvim/.config/nvim/after/ftplugin/cpp.lua
+++ nvim/.config/nvim/after/ftplugin/cpp.lua
@@ -3,5 +3,5 @@ local utils = require("utils")
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*.cpp",
   group = utils.augroup("cpp_runner"),
-  command = utils.runnercmd("cc -o %:p:r % && %:p:r"),
+  command = utils.runnercmd("c++ -o %:p:r % && %:p:r"),
 })
