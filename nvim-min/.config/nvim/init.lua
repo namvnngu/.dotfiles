5deb6ac14a1f497f2bc0f3b387845d2b29b9d4de
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-07-21 01:40:03 +1000

    nvim: init nvim-min to add config when needed

---
+++ nvim-min/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.cmd("colorscheme wildcharm")
+
+vim.o.colorcolumn = "80"
+vim.o.cursorline = true
+vim.o.guicursor = ""
+vim.o.number = true
+vim.o.relativenumber = true
