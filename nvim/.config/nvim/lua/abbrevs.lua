829391ba3ff20a6d67cb0d4b49c4fb75c393f0ca
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-12-01 13:51:43 +1100

    vim+nvim: add abbrevation for C++ competitive programming template

--- nvim/.config/nvim/lua/abbrevs.lua
+++ nvim/.config/nvim/lua/abbrevs.lua
@@ -1,6 +1,6 @@
 vim.keymap.set(
   "ia",
-  "me::",
+  "me@",
   vim.fn.strftime(
     "Author: Nam Nguyen <vnngucs@outlook.com><CR>"
       .. "Description:<CR>"
@@ -8,4 +8,18 @@ vim.keymap.set(
       .. "Date Created: %B %d, %Y<CR>"
       .. "Date Modified: %B %d, %Y"
   )
+)
+
+vim.keymap.set(
+  "ia",
+  "cpcpp@",
+  "#include <iostream><CR>"
+    .. "<CR>"
+    .. "using namespace std;<CR>"
+    .. "<CR>"
+    .. "int main() {<CR>"
+    .. "  ios_base::sync_with_stdio(0);<CR>"
+    .. "  cin.tie(0);<CR>"
+    .. "}"
+    .. "<Esc>gg=GGk^"
 )
