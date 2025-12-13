e6615db56b690461810915df5ef462e3ab1559d0
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-12-13 12:03:09 +1100

    vim+nvim: set b:start for dispatch

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -1,1 +1,2 @@
 vim.b.dispatch = "cc -o %:p:r % && %:p:r"
+vim.b.start = "cc -o %:p:r % && %:p:r"
--- nvim/.config/nvim/after/ftplugin/cpp.lua
+++ nvim/.config/nvim/after/ftplugin/cpp.lua
@@ -1,1 +1,2 @@
 vim.b.dispatch = "c++ -o %:p:r % && %:p:r"
+vim.b.start = "c++ -o %:p:r % && %:p:r"
