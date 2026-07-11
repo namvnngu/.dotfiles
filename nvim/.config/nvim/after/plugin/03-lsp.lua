54ae130ec727a6002360a3a4a38e3957e8968a11
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-07-11 15:57:40 +1000

    nvim: update lua_ls lsp config

--- nvim/.config/nvim/after/plugin/03-lsp.lua
+++ nvim/.config/nvim/after/plugin/03-lsp.lua
@@ -144,13 +144,7 @@ if is_executable({}, "lua-language-server") then
                     },
                     workspace = {
                         checkThirdParty = false,
-                        library = vim.list_extend(
-                            vim.api.nvim_get_runtime_file("", true),
-                            {
-                                "${3rd}/luv/library",
-                                "${3rd}/busted/library",
-                            }
-                        ),
+                        library = vim.api.nvim_get_runtime_file("", true),
                     },
                 })
         end,
