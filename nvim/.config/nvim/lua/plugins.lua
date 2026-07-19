d1d4e9e1403025f9ae64f34ff7a955548429f127
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-07-19 20:53:21 +1000

    nvim: categorize plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,16 +1,18 @@
 vim.pack.add({
-    -- Required
+    -- Base
+    "https://github.com/stevearc/oil.nvim",
     "https://github.com/junegunn/fzf",
+    "https://github.com/tpope/vim-fugitive",
+    "https://github.com/tpope/vim-dispatch",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/stevearc/conform.nvim",
-    "https://github.com/stevearc/oil.nvim",
-    "https://github.com/tpope/vim-dispatch",
-    "https://github.com/tpope/vim-fugitive",
-
-    -- Optional
     "https://github.com/j-hui/fidget.nvim",
     "https://github.com/tommcdo/vim-lion",
+
+    -- Database
     "https://github.com/tpope/vim-dadbod",
     "https://github.com/kristijanhusak/vim-dadbod-ui",
+
+    -- C#
     "https://github.com/Decodetalkers/csharpls-extended-lsp.nvim",
 })
