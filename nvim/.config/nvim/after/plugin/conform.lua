7c4081042db376ac9d4d3b82c29c5f6593ce72ae
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-07-24 12:31:31 +1000

    nvim: call conform.setup

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -4,6 +4,8 @@ if not status then
     return
 end
 
+conform.setup()
+
 vim.keymap.set("n", "<Leader>f", function()
     conform.format({ async = true, lsp_format = "fallback" })
 end, { desc = "Format buffer" })
