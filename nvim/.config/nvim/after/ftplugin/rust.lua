b6f8e9f5d56b8efe6fb03a8a0c97cf89dd2236ff
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-08-01 02:32:49 +1000

    nvim: format files

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -2,7 +2,6 @@ if vim.fn.executable("rust-analyzer") then
     vim.lsp.enable("rust_analyzer")
 end
 
-
 local conform_ok, conform = pcall(require, "conform")
 if conform_ok then
     conform.formatters_by_ft.rust = { "rustfmt" }
