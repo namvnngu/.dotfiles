7d1f8541bb4b2e46b7ca9288305b319dc8eb056e
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-08-07 00:49:11 +1000

    nvim+mise: add joker formatter for Clojure

--- nvim/.config/nvim/after/ftplugin/clojure.lua
+++ nvim/.config/nvim/after/ftplugin/clojure.lua
@@ -1,3 +1,8 @@
 if vim.fn.executable("clojure-lsp") then
     vim.lsp.enable("clojure_lsp")
 end
+
+local conform_ok, conform = pcall(require, "conform")
+if conform_ok then
+    conform.formatters_by_ft.clojure = { "joker" }
+end
